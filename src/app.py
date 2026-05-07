28df6d93f105622a8547dbd160c513e23b30d6ef
buba <brad.raschle@gmail.com>
2026-05-07 14:25:46 +0200

    App initialisiert und getestet

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import pandas as pd
+#import os
+#from dotenv import load_dotenv
+#from src.retrieval.index import load_index
+#from src.retrieval.retrieve import retrieve
+#from src.generation.llm import generate
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+#load_dotenv()
+
+#COLLECTION_NAME = os.getenv('COLLECTION_NAME', 'switzerland_geo')
+#TOP_K = int(os.getenv('TOP_K', 5))
+
+home_page = st.Page("pages/main_page.py", title="GeoRAG")
+
+pg = st.navigation([home_page])
+
+pg.run()
---
+++ src/pages/main_page.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("GeoRAG - Switzerland Geography Assistant")
+st.caption("Ask anything about Swiss geography")
+
